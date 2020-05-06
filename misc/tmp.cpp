2c964ae68f14249584a3af0a99365e2d296f8ec8
Leon <leopard10000@gmail.com>
2020-05-06 22:16:20 +0900

    ABC156-B

---
+++ misc/tmp.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0; i < (n); ++i)
+#define RREP(i, n) for (int i = (n - 1); 0 <= i; --i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+namespace My
+{
+template <typename ArrayT>
+static void DumpArray(const ArrayT &arr)
+{
+#if defined(HOME)
+	for (auto it = std::begin(arr); it != std::end(arr); ++it) {
+		cerr << *it << " ";
+	}
+	cerr << endl;
+#endif
+}
+template<typename T>
+static void DumpGrid(const vector<vector<T>> &grid){
+#if defined(HOME)
+	REP (i, grid.size()) {
+		REP (j, grid[i].size()) {
+			cerr << grid[i][j] << " ";
+		}
+		cerr << endl;
+	}
+#endif
+}
+template <class ArrayT, typename T>
+static void InitArray(ArrayT &arr, T initValue){
+	for (auto &v : arr){
+		v = initValue;
+	}
+}
+template <typename T>
+static bool ReplaceIfSmaller(T &target, T value)
+{
+	if (value < target) {
+		target = value;
+		return true;
+	} else {
+		return false;
+	}
+}
+template <typename T>
+static bool ReplaceIfBigger(T &target, T value)
+{
+	if (target < value) {
+		target = value;
+		return true;
+	} else {
+		return false;
+	}
+}
+
+class Solver
+{
+  public:
+	Solver() {}
+	void Solve(istream &cin, ostream &cout)
+	{
+		int N, K;
+		cin >> N >> K;
+
+		for (size_t i = 0; i < 40; i++)
+		{
+			if (N < pow(K, i)){
+				cout << i << endl;
+				return;
+			}
+		}
+	}
+};
+} // namespace My
+
+#if !defined(BOOST_TEST_MODULE)
+#if !defined(GTEST_INCLUDE_GTEST_GTEST_H_)
+int main()
+{
+	std::cin.tie(0);
+	std::ios::sync_with_stdio(false);
+
+	My::Solver solver;
+	solver.Solve(std::cin, std::cout);
+
+	return 0;
+}
+#endif
+#endif
